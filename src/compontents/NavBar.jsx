d860b85214025e4056e39bb6bdc34772dba3bd1c
Anuj-Khadka <anujkhadka008@gmail.com>
2023-05-30 19:50:30 +0545

    initiate project

---
+++ src/compontents/NavBar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <div className='navbar'>
+        <ul>
+            <li className='nav-item active'></li>
+            <li className='nav-item'></li>
+            <li className='nav-item'></li>
+            <li className='nav-item'></li>
+            <li className='nav-item'></li>
+        </ul>
+    </div>
+  )
+}
+
+export default NavBar
