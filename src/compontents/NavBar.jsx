957638c169e12cda15008e60acd292598679e327
Anuj-Khadka <anujkhadka008@gmail.com>
2023-05-30 23:34:14 +0545

    add navbar

--- src/compontents/NavBar.jsx
+++ src/compontents/NavBar.jsx
@@ -1,17 +1,44 @@
-import React from 'react'
+import React from "react";
+import { NavLink } from "react-router-dom";
+import "../assets/css/NavBar.css";
+import { AiOutlineHome, AiOutlineUser } from "react-icons/ai";
+import { BsTelephone } from "react-icons/bs";
+import { MdOutlineWorkOutline } from "react-icons/md";
+import { RiBook2Line } from "react-icons/ri";
 
 const NavBar = () => {
   return (
-    <div className='navbar'>
-        <ul>
-            <li className='nav-item active'></li>
-            <li className='nav-item'></li>
-            <li className='nav-item'></li>
-            <li className='nav-item'></li>
-            <li className='nav-item'></li>
-        </ul>
+    <div className="navbar">
+      <ul>
+        <li>
+          <NavLink to="/" className="nav-item">
+            <AiOutlineHome />
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/about" className="nav-item">
+            <AiOutlineUser />
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/projects" className="nav-item">
+            <MdOutlineWorkOutline />
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/blogs" className="nav-item">
+            <RiBook2Line />
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/contact" className="nav-item">
+            <BsTelephone />
+          </NavLink>
+        </li>
+        {/* <NavLink className='nav-item'></NavLink> */}
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
