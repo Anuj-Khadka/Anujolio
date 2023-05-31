20fbbf30d4ea0bcb67f28ec82b686ac526bf73a9
Anuj-Khadka <anujkhadka008@gmail.com>
2023-05-31 23:07:43 +0545

    update project page

--- src/compontents/Projects.jsx
+++ src/compontents/Projects.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import ProjectsList from "../apis/projects-api.json"
 
 const Projects = () => {
+  fetch(ProjectsList)
+  .then(response=> console.log(response))
+  .catch(error => console.log("eooror", error))
   return (
-    <div>Projects</div>
+    <div>
+      Projects
+    </div>
   )
 }
 
