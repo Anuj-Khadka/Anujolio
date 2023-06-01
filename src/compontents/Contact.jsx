b5f27b44521ffc3a17fc7eda0e30ebcfad73d0f2
Anuj-Khadka <anujkhadka008@gmail.com>
2023-06-01 21:56:38 +0545

    update contact page

--- src/compontents/Contact.jsx
+++ src/compontents/Contact.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import "../assets/css/Contact.css";
 import ContactForm from "./micro-components/ContactForm";
+import {MdLocationOn, MdEmail} from "react-icons/md"
+import { BsTelephone,  } from "react-icons/bs";
 
 const Contact = () => {
   return (
@@ -12,48 +14,41 @@ const Contact = () => {
       <div className="contact">
         <div className="contact-row">
           <div className="form-container">
-            {/* <img src={Anuj} alt="anuj-khadka" /> */}
+            <h5>Contact Me</h5>
             <ContactForm />
           </div>
           <div className="contact-info-container">
             {/* <div className="nice-greeting">Nice meeting you!</div> */}
             <h5>Contact Info</h5>
             <div className="contact-info-row">
-                <div className="contact-info-item">
-                  <span>
-                    {/* <BsPersonCircle /> */}
-                  </span>
-                  <p className="tertiary">Anuj Khadka</p>
-                </div>
-                <div className="contact-info-item">
-                  <span>
-                    {/* <MdLocationOn /> */}
-                  </span>
-                  <p className="tertiary">Dhankuta, Nepal</p>
-                </div>
-                <div className="contact-info-item">
-                  <span>
-                    {/* <MdEmail /> */}
-                  </span>
-                  <p className="tertiary">
-                    <a
-                      href="mailto:anujkhadka008@gmail.com"
-                      class="tertiary"
-                      style={{ textDecoration: "underline" }}
-                    >
-                      anujkhadka008@gmail.com
-                    </a>
-                  </p>
-                </div>
-                <div className="contact-info-item">
-                  <span>
-                    {/* <GrStatusCriticalSmall /> */}
-                  </span>
-                  <p className="tertiary">Open for Work</p>
-                </div>
+              <div className="contact-info-item">
+                <span><MdLocationOn /></span>
+                <p className="tertiary">Dhankuta, Nepal</p>
               </div>
+              <div className="contact-info-item">
+                <span><BsTelephone /></span>
+                <p className="tertiary">+977-9823048067</p>
+              </div>
+
+              <div className="contact-info-item">
+                <span><MdEmail /> </span>
+                <p className="tertiary">
+                  <a
+                    href="mailto:anujkhadka008@gmail.com"
+                    class="tertiary"
+                    style={{ textDecoration: "underline" }}
+                  >
+                    anujkhadka008@gmail.com
+                  </a>
+                </p>
+              </div>
+              {/* <div className="contact-info-item">
+                <span><GrStatusCriticalSmall /> </span>
+                <p className="tertiary">Open for Work</p>
+              </div> */}
+            </div>
             {/* <div className="seperator"></div>  */}
-          </div> 
+          </div>
         </div>
       </div>
     </div>
