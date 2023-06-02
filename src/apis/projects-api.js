e16261e20a975f4a175bcd1deb1ace89ba9f00a0
Anuj-Khadka <anujkhadka008@gmail.com>
2023-06-02 23:26:16 +0545

    update projects ist

--- src/apis/projects-api.js
+++ src/apis/projects-api.js
@@ -1,45 +1,73 @@
 // const jov_img = require("../assets/images/project-images/jovai.PNG")
 
-// import jov_img from "../assets/images/project-images/jovai.PNG";
+import jov_img from "../assets/images/project-images/jovai.png";
 import frida_img from "../assets/images/project-images/frida.png";
-// import jelly_img from "../assets/images/project-images/jelly.PNG";
-// import setbootstrap_img from "../assets/images/project-images/setbootstrap.PNG";
+import jelly_img from "../assets/images/project-images/jelly.png";
+import setbootstrap_img from "../assets/images/project-images/setbootstrap.png";
+import mathify_img from "../assets/images/project-images/mathify.png";
 
 const ProjectsList = [
   {
     project_name: "JovAi",
-    project_image: frida_img,
+    project_image: jov_img,
     project_url: "https://jovai.netlify.app",
     github_link: "https://github.com/anuj-khadka",
     project_type: "website",
     project_desc:
-      "lorem ipsum this is all about my project I don't know if this site is correct or not but ya it works out.",
-    project_stack: "HTML, CSS, Bootstrap, JS, React, Redux"
+      "Built a complete website that renders an image, captured by Juno Spacecraft, of Jupiter, and also fetches the black and white images comparing its optimized image sidewise.",
+    project_stack: "HTML, CSS, Bootstrap, JS, PyTorch"
   },
   {
     project_name: "Frida",
     project_image: frida_img,
     project_url: "https://jovai.netlify.app",
     github_link: "https://github.com/anuj-khadka",
-    project_type: "website",
+    project_type: "mobile application",
     project_desc:
-      "lorem ipsum this is all about my project I don't know if this site is correct or not but ya it works out.",
+      "A one-stop solution to all your climate action needs that predicts landslides and flood events and provides news summaries, fundraising, and upcoming events alerts on climate change.",
     project_stack: "HTML, CSS, Bootstrap, JS, React, Redux"
   },
   {
     project_name: "Jelly",
-    project_image: frida_img,
-    project_url: "https://jovai.netlify.app",
+    project_image: jelly_img,
+    project_url: "https://jelly.thetwl.org",
+    github_link: "https://github.com/anuj-khadka/jelly",
+    project_type: "website",
+    project_desc:
+      "elly can understand romanized Nepali with English alphabets and would talk to you in the same. It is Nepal's first romanized chatbot, which has 1000+ active users, helping 90+ people to cure their boredom and loneliness.",
+    project_stack: "HTML, CSS, Bootstrap, JS, React, Redux"
+  },
+  {
+    project_name: "Set Bootstrap",
+    project_image: setbootstrap_img,
+    project_url: "https://setbootstrapy.netlify.app",
     github_link: "https://github.com/anuj-khadka",
     project_type: "website",
     project_desc:
       "lorem ipsum this is all about my project I don't know if this site is correct or not but ya it works out.",
     project_stack: "HTML, CSS, Bootstrap, JS, React, Redux"
-  },
-  {
+  },{
+    project_name: "Mathify",
+    project_image: mathify_img,
+    project_url: "https://themathify.netlify.app/",
+    github_link: "https://github.com/anuj-khadka/mathify",
+    project_type: "website",
+    project_desc:
+      "A beginner project calculator, Mathify made with React.js is a simple application that allows users to perform basic mathematical calculations.",
+    project_stack: "HTML, CSS, Bootstrap, JS, React, Redux"
+  },{
     project_name: "Set Bootstrap",
-    project_image: frida_img,
-    project_url: "https://jovai.netlify.app",
+    project_image: setbootstrap_img,
+    project_url: "https://setbootstrapy.netlify.app",
+    github_link: "https://github.com/anuj-khadka",
+    project_type: "website",
+    project_desc:
+      "lorem ipsum this is all about my project I don't know if this site is correct or not but ya it works out.",
+    project_stack: "HTML, CSS, Bootstrap, JS, React, Redux"
+  },{
+    project_name: "Set Bootstrap",
+    project_image: setbootstrap_img,
+    project_url: "https://setbootstrapy.netlify.app",
     github_link: "https://github.com/anuj-khadka",
     project_type: "website",
     project_desc:
