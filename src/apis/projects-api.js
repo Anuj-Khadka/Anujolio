262b963c93edb07cfbacd2fbdd9918e87eea66b5
Anuj-Khadka <anujkhadka008@gmail.com>
2023-06-02 21:59:28 +0545

    update projects list

--- src/apis/projects-api.js
+++ src/apis/projects-api.js
@@ -11,24 +11,40 @@ const ProjectsList = [
     project_image: frida_img,
     project_url: "https://jovai.netlify.app",
     github_link: "https://github.com/anuj-khadka",
+    project_type: "website",
+    project_desc:
+      "lorem ipsum this is all about my project I don't know if this site is correct or not but ya it works out.",
+    project_stack: "HTML, CSS, Bootstrap, JS, React, Redux"
   },
   {
     project_name: "Frida",
     project_image: frida_img,
     project_url: "https://jovai.netlify.app",
     github_link: "https://github.com/anuj-khadka",
+    project_type: "website",
+    project_desc:
+      "lorem ipsum this is all about my project I don't know if this site is correct or not but ya it works out.",
+    project_stack: "HTML, CSS, Bootstrap, JS, React, Redux"
   },
   {
     project_name: "Jelly",
     project_image: frida_img,
     project_url: "https://jovai.netlify.app",
     github_link: "https://github.com/anuj-khadka",
+    project_type: "website",
+    project_desc:
+      "lorem ipsum this is all about my project I don't know if this site is correct or not but ya it works out.",
+    project_stack: "HTML, CSS, Bootstrap, JS, React, Redux"
   },
   {
     project_name: "Set Bootstrap",
     project_image: frida_img,
     project_url: "https://jovai.netlify.app",
     github_link: "https://github.com/anuj-khadka",
+    project_type: "website",
+    project_desc:
+      "lorem ipsum this is all about my project I don't know if this site is correct or not but ya it works out.",
+    project_stack: "HTML, CSS, Bootstrap, JS, React, Redux"
   },
 ];
 
