e72dcaa455adaedff5169a0aaef7a0f1557e01f3
Anuj-Khadka <anujkhadka008@gmail.com>
2023-06-03 21:42:03 +0545

    update projects list

--- src/apis/projects-api.js
+++ src/apis/projects-api.js
@@ -1,31 +1,73 @@
 // const jov_img = require("../assets/images/project-images/jovai.PNG")
 
+import kinmel_img from "../assets/images/project-images/kinmel.png";
+import memorivise_img from "../assets/images/project-images/memorivise.png";
+import mgc_img from "../assets/images/project-images/mgc.png";
 import jov_img from "../assets/images/project-images/jovai.png";
 import frida_img from "../assets/images/project-images/frida.png";
 import jelly_img from "../assets/images/project-images/jelly.png";
 import setbootstrap_img from "../assets/images/project-images/setbootstrap.png";
 import mathify_img from "../assets/images/project-images/mathify.png";
+import twl_img from "../assets/images/project-images/twl.png";
+import google_img from "../assets/images/project-images/google.png";
+import facebook_img from "../assets/images/project-images/facebook.png";
+import reacttodo_img from "../assets/images/project-images/reacttodo.png";
+import weirdrecipe_img from "../assets/images/project-images/weirdrecipe.png";
+import essaycomp_img from "../assets/images/project-images/essay-comp.png";
+import wiki_img from "../assets/images/project-images/wiki.jpg";
 
 const ProjectsList = [
+  {
+    project_name: "Kinmel",
+    project_image: kinmel_img,
+    project_url: "https://hamrokinmel.netlify.app/",
+    github_link: "https://github.com/anuj-khadka/Kinmel",
+    project_type: "website",
+    project_desc:
+      "Built a complete website that renders an image, captured by Juno Spacecraft, of Jupiter, and also fetches the black and white images comparing its optimized image sidewise.",
+    project_stack: "HTML, CSS, JS, React, Redux, Fake Store Api",
+  },
+  {
+    project_name: "Memorivise",
+    project_image: memorivise_img,
+    project_url: "https://anuj-khadka.com.np/projects/",
+    github_link: "https://github.com/anuj-khadka/Memorivise",
+    project_type: "website",
+    project_desc:
+      "Built a complete website that renders an image, captured by Juno Spacecraft, of Jupiter, and also fetches the black and white images comparing its optimized image sidewise.",
+    project_stack: "HTML, CSS, Bootstrap, JS, Python, Flask, SQLite",
+  },
+  {
+    project_name: "Music Genre Classifier",
+    project_image: mgc_img,
+    project_url:
+      "https://github.com/Anuj-Khadka/Music-Streaming-App-Genre-Classification",
+    github_link:
+      "https://github.com/Anuj-Khadka/Music-Streaming-App-Genre-Classification",
+    project_type: "website",
+    project_desc:
+      "Built a complete website that renders an image, captured by Juno Spacecraft, of Jupiter, and also fetches the black and white images comparing its optimized image sidewise.",
+    project_stack: "HTML, CSS, Bootstrap, JS, , Python, Django, Sklearn, Tensorflow",
+  },
   {
     project_name: "JovAi",
     project_image: jov_img,
     project_url: "https://jovai.netlify.app",
-    github_link: "https://github.com/anuj-khadka",
+    github_link: "https://github.com/Anuj-Khadka/Nepali-Ho-Ni_NASA-SpaceApp",
     project_type: "website",
     project_desc:
       "Built a complete website that renders an image, captured by Juno Spacecraft, of Jupiter, and also fetches the black and white images comparing its optimized image sidewise.",
-    project_stack: "HTML, CSS, Bootstrap, JS, PyTorch"
+    project_stack: "HTML, CSS, Bootstrap, JS, PyTorch",
   },
   {
     project_name: "Frida",
     project_image: frida_img,
     project_url: "https://jovai.netlify.app",
-    github_link: "https://github.com/anuj-khadka",
+    github_link: "https://github.com/Anuj-Khadka/FRIDA-TEAM_AIR-KU-hackfest",
     project_type: "mobile application",
     project_desc:
       "A one-stop solution to all your climate action needs that predicts landslides and flood events and provides news summaries, fundraising, and upcoming events alerts on climate change.",
-    project_stack: "HTML, CSS, Bootstrap, JS, React, Redux"
+    project_stack: "Firebase, Flutter, Figma",
   },
   {
     project_name: "Jelly",
@@ -35,7 +77,47 @@ const ProjectsList = [
     project_type: "website",
     project_desc:
       "elly can understand romanized Nepali with English alphabets and would talk to you in the same. It is Nepal's first romanized chatbot, which has 1000+ active users, helping 90+ people to cure their boredom and loneliness.",
-    project_stack: "HTML, CSS, Bootstrap, JS, React, Redux"
+    project_stack: "HTML, CSS, Bootstrap, JS, Python, Flask, Hugging Face",
+  },
+  {
+    project_name: "React ToDo App",
+    project_image: reacttodo_img,
+    project_url: "https://react-toodoos.netlify.app/",
+    github_link: "https://github.com/Anuj-Khadka/react-todo-app",
+    project_type: "website",
+    project_desc:
+      "elly can understand romanized Nepali with English alphabets and would talk to you in the same. It is Nepal's first romanized chatbot, which has 1000+ active users, helping 90+ people to cure their boredom and loneliness.",
+    project_stack: "HTML, CSS, Bootstrap, JS, React, Redux",
+  },
+  {
+    project_name: "Weird Recipe App",
+    project_image: weirdrecipe_img,
+    project_url: "https://weird-recipe.netlify.app/",
+    github_link: "https://github.com/Anuj-Khadka/react-recipe-app",
+    project_type: "website",
+    project_desc:
+      "elly can understand romanized Nepali with English alphabets and would talk to you in the same. It is Nepal's first romanized chatbot, which has 1000+ active users, helping 90+ people to cure their boredom and loneliness.",
+    project_stack: "HTML, CSS, Bootstrap, JS, React, Redux",
+  },
+  {
+    project_name: "Wiki",
+    project_image: wiki_img,
+    project_url: "https://github.com/Anuj-Khadka/wiki",
+    github_link: "https://github.com/Anuj-Khadka/wiki",
+    project_type: "website",
+    project_desc:
+      "elly can understand romanized Nepali with English alphabets and would talk to you in the same. It is Nepal's first romanized chatbot, which has 1000+ active users, helping 90+ people to cure their boredom and loneliness.",
+    project_stack: "HTML, CSS, Bootstrap, JS, React, Redux",
+  },
+  {
+    project_name: "Essay Competition Portal",
+    project_image: essaycomp_img,
+    project_url: "https://essay-competition.thetwl.org/",
+    github_link: "https://github.com/Anuj-Khadka/essay-competition",
+    project_type: "website",
+    project_desc:
+      "elly can understand romanized Nepali with English alphabets and would talk to you in the same. It is Nepal's first romanized chatbot, which has 1000+ active users, helping 90+ people to cure their boredom and loneliness.",
+    project_stack: "HTML, CSS, Bootstrap, JS, React, Redux",
   },
   {
     project_name: "Set Bootstrap",
@@ -45,8 +127,9 @@ const ProjectsList = [
     project_type: "website",
     project_desc:
       "lorem ipsum this is all about my project I don't know if this site is correct or not but ya it works out.",
-    project_stack: "HTML, CSS, Bootstrap, JS, React, Redux"
-  },{
+    project_stack: "HTML, CSS, Bootstrap, JS, React, Redux",
+  },
+  {
     project_name: "Mathify",
     project_image: mathify_img,
     project_url: "https://themathify.netlify.app/",
@@ -54,25 +137,37 @@ const ProjectsList = [
     project_type: "website",
     project_desc:
       "A beginner project calculator, Mathify made with React.js is a simple application that allows users to perform basic mathematical calculations.",
-    project_stack: "HTML, CSS, Bootstrap, JS, React, Redux"
-  },{
-    project_name: "Set Bootstrap",
-    project_image: setbootstrap_img,
-    project_url: "https://setbootstrapy.netlify.app",
+    project_stack: "HTML, CSS, Bootstrap, JS, React, Redux",
+  },
+  {
+    project_name: "Together We Learn",
+    project_image: twl_img,
+    project_url: "https://thetwl.org",
     github_link: "https://github.com/anuj-khadka",
     project_type: "website",
     project_desc:
-      "lorem ipsum this is all about my project I don't know if this site is correct or not but ya it works out.",
-    project_stack: "HTML, CSS, Bootstrap, JS, React, Redux"
-  },{
-    project_name: "Set Bootstrap",
-    project_image: setbootstrap_img,
-    project_url: "https://setbootstrapy.netlify.app",
-    github_link: "https://github.com/anuj-khadka",
+      "Providing resources, ideas and webinars to new coders, designers, potential data scientist and machine learning engineers all for free.",
+    project_stack: "HTML, CSS, JS, WordPress, PHP",
+  },
+  {
+    project_name: "Google Landing Page Clone",
+    project_image: google_img,
+    project_url: "https://github.com/Anuj-Khadka/Google-Landing-Page",
+    github_link: "https://github.com/Anuj-Khadka/Google-Landing-Page",
     project_type: "website",
     project_desc:
       "lorem ipsum this is all about my project I don't know if this site is correct or not but ya it works out.",
-    project_stack: "HTML, CSS, Bootstrap, JS, React, Redux"
+    project_stack: "HTML, CSS, Bootstrap, JS, React, Redux",
+  },
+  {
+    project_name: "Facebook Login Clone",
+    project_image: facebook_img,
+    project_url: "https://facebooklogin-frontend.netlify.app",
+    github_link: "https://github.com/Anuj-Khadka/Facebook-Login-Page-",
+    project_type: "website",
+    project_desc:
+      "lorem ipsum this is all about my project I don't know if this site is correct or not but ya it works out.",
+    project_stack: "HTML, CSS, Bootstrap, JS, React, Redux",
   },
 ];
 
